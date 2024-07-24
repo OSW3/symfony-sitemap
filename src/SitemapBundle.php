0d5e6721662feaf40f95670b85587f86618aef86
Arnaud BODEL <arnaud.bodel@gmail.com>
2024-07-25 01:19:55 +0200

    -

--- src/SitemapBundle.php
+++ src/SitemapBundle.php
@@ -1,14 +1,14 @@
 <?php 
 namespace OSW3\Sitemap;
 
-use OSW3\Sitemap\Utils\ConfigurationYaml;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use OSW3\Sitemap\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class SitemapBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        ConfigurationYaml::write($container->getParameter('kernel.project_dir'));
+        (new Configuration)->generateProjectConfig($container->getParameter('kernel.project_dir'));
     }
 }
